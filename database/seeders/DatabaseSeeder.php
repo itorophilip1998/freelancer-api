aa0775ccdceffd4c3df47392a1b0965d0ded7154
itphilip1998 <itkeyzzpianny@gmail.com>
2022-05-26 11:34:42 +0100

    stagging

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserDetailsSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create();
+       UserDetailsSeeder::class;
     }
 }
---
+++ database/seeders/UserDetailsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserDetailsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+ 
+           User::create( ['name' => "Admin",
+            'email' => "admin@gmail.com",
+            'email_verified_at' => now(),
+            'phone' => "09024195493",
+            'role'=>"admin",
+            'password' =>bcrypt("admin@2022"),  
+            'remember_token' => Str::random(10) ]);
+    
+    }
+
+    //seller email:websoftLTD@gmail.com  password:password123
+    //buyer email: buyer@gmail.com ,password:password123
+   
+}
