db72485b204e6251c56b5fbca077e96beea933d0
itphilip1998 <itkeyzzpianny@gmail.com>
2022-06-23 13:05:47 +0100

    setUp

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -42,38 +42,39 @@ class SkillController extends Controller
  public function update($id)
  {
       try {
-
-                $validator = Validator::make(request()->all(), [
+            $validator = Validator::make(request()->all(), [
                 'name' => 'required|string', 
                 'rate' => 'required|integer', 
                 'user_id' => 'required|string'
             ]);
             if ($validator->fails()) {
                             return response()->json($validator->errors(), 422);
-                        }
+              }
             if(!auth()->check()){
                 return response()->json(['message' => 'Unauthorized ⚠️'], 401);
             } 
           $skill=Skill::where("id",$id)
-          ->where("user_id",auth()->user()["id"]);
+          ->where("user_id",request()->user_id);
           $skill->update(["name"=>request()->name,"rate"=>request()->rate]);
           if(!$skill)  return response()->json(['message' => 'Sorry this skill does not belong to you or does not exist⚠️','skill'=>$skill],401); 
-             $newskill=Skill::where("id",$id)->first();
+             $newskill=Skill::where("id",$id)
+                  ->where("user_id",request()->user_id)
+                  ->first();
           return response()->json(['message' => 'Skill successfully Update 👍','skill'=>$newskill],200); 
 
       } catch (\Throwable $th) {
-          throw $th;
+        //   throw $th;
           return response()->json([
            'message' => 'This error is from the backend, please contact the backend developer'],500);
-        
       }
  }
+ 
  public function get($user_id)
  {
       try {
             if(!auth()->check()){
                 return response()->json(['message' => 'Unauthorized ⚠️'], 401);
-            }  
+            }
         $skill=Skill::where("user_id",$user_id)->get();
         return response()->json(['message' => 'Skill successfully Loaded 👍','skill'=>$skill],200); 
       } catch (\Throwable $th) {
