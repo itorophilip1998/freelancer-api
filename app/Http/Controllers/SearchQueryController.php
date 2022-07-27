9f5842c95b4144c9e781043bcdc4dc82b43d27f0
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-27 17:02:48 +0100

    dev

--- app/Http/Controllers/SearchQueryController.php
+++ app/Http/Controllers/SearchQueryController.php
@@ -28,28 +28,27 @@ class SearchQueryController extends Controller
                 ->get()
                 ->map(
                     function ($data) {
-                        // $ranting = $data["ratings"];
-                        // if (count($ranting) === 0) {
-                        //     return response()->json(['message' => 'Sorry this review does not belong to you or does not exist⚠️', 'ratings' => $ranting], 401);
-                        // }
-                        // $arr = $ranting;
-                        // $count = 0;
-                        // $sum = 0;
-                        // $index = 0;
-                        // foreach ($arr as $item) {
-                        //     $count += $item["rate"];
-                        //     $sum += $item["rate"] * ($index += 1);
-                        // }
-                        // $star = $sum / $count;
-                        // $rate = strlen($star) > 3 ? substr($star, 0, 3)  : $star;
-
-                        // $data['rate_star'] = floatval($rate);
+                        $ranting = $data["ratings"];
+                        if (count($ranting) === 0) {
+                            return response()->json(['message' => 'Sorry this review does not belong to you or does not exist⚠️', 'ratings' => $ranting], 401);
+                        }
+                        $arr = $ranting;
+                        $count = 0;
+                        $sum = 0;
+                        $index = 0;
+                        foreach ($arr as $item) {
+                            $count += $item["rate"];
+                            $sum += $item["rate"] * ($index += 1);
+                        }
+                        $star = $sum / $count;
+                        $rate = strlen($star) > 3 ? substr($star, 0, 3)  : $star; 
+                        $data['rate_star'] = floatval($rate);
                         return $data;
                     }
                 );
 
             return  response()->json([
-                "message" => "Searched data loaded!",  "data" => $userBySkill
+                "message" => "Searched data loaded!","data" => $userBySkill
             ], 200);
         } catch (\Throwable $th) {
             throw $th;
