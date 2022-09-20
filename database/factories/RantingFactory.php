dc0485384d85ffbc838c2586d3641cb8cb844b20
itorophilip1998 <itorophilip1998@gmail.com>
2022-09-20 15:57:13 +0100

    dev

--- database/factories/RantingFactory.php
+++ database/factories/RantingFactory.php
@@ -15,8 +15,8 @@ class RantingFactory extends Factory
     {
         return [
             "rate"=>rand(1,5),
-            "user_id"=>rand(1,50),
-            "rater_id"=>rand(51,100),
+            "rater_id"=>rand(1,50),
+            "user_id"=>rand(51,100),
             "reviews"=>$this->faker->title,
         ];
     }
