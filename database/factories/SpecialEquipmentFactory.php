b31aaf6ec2d0f7a89589f01ed61b6ae17c0cd58a
itorophilip1998 <itorophilip1998@gmail.com>
2022-09-20 14:47:42 +0100

    special equipment

--- database/factories/SpecialEquipmentFactory.php
+++ database/factories/SpecialEquipmentFactory.php
@@ -13,10 +13,10 @@ class SpecialEquipmentFactory extends Factory
      */
     public function definition()
     {
-        
+
         return [
             'user_id' => rand(1, 100),
-            'skill_id' => rand(1, 100),
+            'skill_id' => rand(20, 100),
             'name'   => $this->faker->jobTitle,
             'rate' => rand(10, 1000),
         ];
